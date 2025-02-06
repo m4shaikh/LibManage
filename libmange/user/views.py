40164d2d7a431dc5b61fd39a4e13e173d2db0e2b
m4shaikh <moin.shaikh.3656@gmail.com>
2025-02-06 21:28:49 +0530

    Created views and templates for login and Signup

--- libmange/user/views.py
+++ libmange/user/views.py
@@ -1,3 +1,44 @@
-from django.shortcuts import render
-
+from django.shortcuts import render , redirect
+from django.http import HttpResponse
+from .models import User
+from django.contrib.auth import authenticate , login , logout
+ 
 # Create your views here.
+
+def signup(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        email = request.POST['email']
+        password = request.POST['password']
+        confirm_password = request.POST['confirm_password']
+        phone = request.POST['phone']
+        
+        if password == confirm_password:
+            user = User.objects.create_user(
+                username = username,
+                email = email ,
+                password = password,
+                phone = phone
+            )    
+            login (request , user)
+            return HttpResponse('success')
+        else:
+            return HttpResponse ('passworrd Do Not Match')
+    render (request , 'user/signup.html')
+    
+    
+def login(request):
+    
+    if request.method == 'POST':
+        username = request.POST['username']
+        password = request.POST['password']
+        user = authenticate(request , username = username , password = password)
+        
+        if user is not None:
+            login (request , user)
+            return HttpResponse('OK')
+        
+        else: 
+            return HttpResponse('Failed')
+    render (request , 'user/login.html')
